b16a1c3725cf837ec15a98f1b2fc56326854dbe6
Juri Borges Falcão Neto <jurineto@gmail.com>
2025-08-18 21:52:59 -0300

    Preenchimento da classe abstrata com os campos solicitados e iniciação

--- src/main/java/cms/user/User.java
+++ src/main/java/cms/user/User.java
@@ -2,5 +2,20 @@ package cms.user;
 
 public abstract class User {
 
+	private int id;
+	private String nome;
+	private String email;
+	private String password;
 	
+public User(int id, String nome, String email, String password) {
+	
+	this.id = id;
+	this.nome = nome;
+	this.email = email;
+	this.password = password;
+	
+}
+
+
+
 }
